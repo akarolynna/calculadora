6b839f2712445db5d2776fd85c449bd2b62421bc
Anna Karolynna <126431104+akarolynna@users.noreply.github.com>
2024-09-03 11:16:33 -0300

    Esqueleto do Projeto

---
+++ calculadora/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const TelaInicial());
+}
+
+class TelaInicial extends StatelessWidget {
+  const TelaInicial({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primaryColor: const Color(0XFFFF4F5B)),
+      home: Column(
+          children: [
+            Center(
+              //Container da imagem
+              child:SizedBox(
+                
+                width: 421,
+                height: 385,
+                child: Image.asset('assets/imagens/'),
+
+
+              ),
+            )
+          ],
+        ),
+      
+     
+
+    );
+  }
+}
